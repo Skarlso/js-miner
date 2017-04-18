19da9c0d6a4c7c41ae138773fa924f5b86a716c5
Gergely Brautigam <skarlso777@gmail.com>
2017-04-18 17:03:04 +0200

    Starting to add tests.

---
+++ tests/docker_test.js
@@ -0,0 +1,14 @@
+var expect = require('chai').expect
+var dockerHelpet = require('../utils/docker')
+
+describe('Docker Helper', function () {
+  describe('#setup', function () {
+    it('sets up a given version', function () {
+    })
+  })
+
+  describe('Hex to RGB conversion', function () {
+    it('converts the basic colors', function () {
+    })
+  })
+})
---
+++ tests/version_test.js
@@ -0,0 +1,9 @@
+var expect = require('chai').expect;
+var v = require('../utils/version.js');
+
+describe("Version File Generator", function() {
+  describe("#setVersion", function() {
+    it("sets up a given version", function() {
+    });
+  });
+});
